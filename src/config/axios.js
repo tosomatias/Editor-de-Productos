bd744eb821c9c8d4194eb9fa9b2101a7e357c1ab
Ignacio Albrieu <ialbrieu1@gmail.com>
2020-10-26 20:14:06 -0300

    Fix axios setup

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const clienteAxios = axios.create({
     baseURL: ' http://localhost:4000/'
+    baseURL: 'https://github.com/tosomatias/Editor-de-Productos'
 });
 
 export default clienteAxios;
